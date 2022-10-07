3371a7a96895d3ea6ac78cee7069ab61e078653b
Doston Kanaev <d.kanaev@sda.local>
2022-10-07 17:49:18 +0300

    first module completed

--- logic.py
+++ logic.py
@@ -83,31 +83,53 @@ def elasticity_calculating(duty:DataFrame,imp:DataFrame,skp:list): #calculating 
     return elast
 
 def adding_new_duties_to_df(data:DataFrame,products:list,duties:list,years:ndarray): #adding new duties from user to DF (lists from Djobir)
-    new_duties = data[data['year_number'] == years[len(years)-2]]
+    new_duties = data[data['year_number'] == years[-2]]
     new_duties.set_index('product_name',drop=False,inplace=True)
     j = 0
     for i in products:
         new_duties.loc[i,'duty'] = duties[j]
         j += 1
     new_duties.reset_index(drop=True,inplace=False)
-    new_duties.loc[:,'year_number'] = years[len(years)-1]
+    new_duties.loc[:,'year_number'] = years[-1]
     data = pd.concat([data,new_duties])
     return data
 
 def adding_new_import(years:ndarray,skp:list,elasticity:ndarray,alpha:float,duty:DataFrame,imp:DataFrame): #import forecast for the new year
     j = 0
     for i in skp:
-        new_duty = duty.loc[(duty['skp'] == i) & (duty['year'] == years[len(years)-1])]
-        old_duty = duty.loc[(duty['skp'] == i) & (duty['year'] == years[len(years)-2])]
-        old_import = imp.loc[(imp['skp'] == i) & (imp['year'] == years[len(years)-2])]
-        imp.loc[(len(imp.index))] = [i,(1 + alpha + elasticity[j] * (new_duty['value'] - old_duty['value']))*old_import['value'],years[len(years)-1]]
+        new_duty = duty.loc[(duty['skp'] == i) & (duty['year'] == years[-1])]
+        old_duty = duty.loc[(duty['skp'] == i) & (duty['year'] == years[-2])]
+        old_import = imp.loc[(imp['skp'] == i) & (imp['year'] == years[-2])]
+        new_duty = new_duty['value'].values
+        old_duty = old_duty['value'].values
+        old_import = old_import['value'].values
+        new_value = (1 + alpha + elasticity[j] * (new_duty[0] - old_duty[0]))*old_import[0]
+        imp.loc[(len(imp.index))] = [i,new_value,years[-1]]
         j += 1
     return imp
 
+def first_modul_main(countries:list,skp:list,products:list,duties:list,user_year:int,alpha:float): #we need alpha and end year of prediction
+    data = data_reading(countries,skp)
+    years = year(data)
+    duty = creating_duties(years,data,skp)
+    imp = creating_import(years,data,skp)
+    elasticity = elasticity_calculating(duty,imp,skp)
+    starting_point = years[-1]
+    for i in range(starting_point,user_year):
+        years = np.append(years,i+1)
+        data = adding_new_duties_to_df(data,products,duties,years)
+        duty = creating_duties(years,data,skp)
+        imp = adding_new_import(years,skp,elasticity,alpha,duty,imp)
+        elasticity = elasticity_calculating(duty,imp,skp)
+    return imp
 
 
 if __name__ == '__main__':
     country_id = ['Армения','Беларусь','Казахстан','Кыргызстан','Российская Федерация']
     skp = ['C13','C14','C15','C21','C29','C30']
-    duties = [5,12,25]
-    products = ['МЕХ ИСКУССТВЕННЫЙ И ИЗДЕЛИЯ ИЗ НЕГО','ШЕЛК-СЫРЕЦ (НЕКРУЧЕНЫЙ)','ПРЯЖА ИЗ ШЕРСТИ ИЛИ ТОНКОГО ВОЛОСА ЖИВОТНЫХ, РАСФАСОВАННАЯ ДЛЯ РОЗНИЧНОЙ ПРОДАЖИ:С СОДЕРЖАНИЕМ ШЕРСТИ ИЛИ ТОНКОГО ВОЛОСА ЖИВОТНЫХ 85 МАС.% ИЛИ БОЛЕЕ:ПРОЧАЯ']
+    duties = [5,12,25,7,10,15]
+    products = ['МЕХ ИСКУССТВЕННЫЙ И ИЗДЕЛИЯ ИЗ НЕГО','ПРЕДМЕТЫ ОДЕЖДЫ И ПРИНАДЛЕЖНОСТИ К ОДЕЖДЕ, ИЗ НАТУРАЛЬНОЙ КОЖИ ИЛИ КОМПОЗИЦИОННОЙ КОЖИ:ПРЕДМЕТЫ ОДЕЖДЫ:ИЗ НАТУРАЛЬНОЙ КОЖИ','ДУБЛ.КОЖА ИЛИ КОЖЕВЕН.КРАСТ ИЗ ШКУР К.Р.С.(ВКЛ. БУЙВОЛ),ЖИВОТ.СЕМ-ВА ЛОШАДИНЫХ,...:ВО ВЛАЖНОМ СОСТ.(ВКЛ.ХРОМИРОВАНН.ПОЛУФАБРИКАТ):НЕШЛИФОВ.ЛИЦЕВЫЕ НЕДВОЕНЫЕ;ЛИЦЕВЫЕ ДВОЕНЫЕ:ИЗ ЦЕЛЫХ ШКУР К.Р.С.(ВКЛ.БУЙВОЛ),ПЛОЩАДЬ ПОВЕРХН.КОТ.< 2,6 М2 (28 КВАДРАТ.ФУТОВ)',
+    'КИСЛОТЫ КАРБОНОВЫЕ, СОДЕРЖАЩИЕ ФЕНОЛЬНУЮ ГРУППУ, НО НЕ СОДЕРЖАЩИЕ ДРУГУЮ КИСЛОРОДСОДЕРЖАЩУЮ ФУНКЦИОНАЛЬНУЮ ГРУППУ, ИХ АНГИДРИДЫ, ГАЛОГЕНАНГИДРИДЫ, ПЕРОКСИДЫ, ПЕРОКСИКИСЛОТЫ И ИХ ПРОИЗВОДНЫЕ: САЛИЦИЛОВАЯ КИСЛОТА И ЕЕ СОЛИ',
+    'ПРОЧИЕ ИЗДЕЛИЯ ИЗ СВИНЦА:ПРОЧИЕ','ЖЕЛЕЗНОДОРОЖНЫЕ ЛОКОМОТИВЫ, С ПИТАНИЕМ ОТ ВНЕШНЕГО ИСТОЧНИКА ЭЛЕКТРОЭНЕРГИИ, ИЛИ АККУМУЛЯТОРНЫЕ: С ПИТАНИЕМ ОТ ЭЛЕКТРИЧЕСКИХ АККУМУЛЯТОРОВ']
+    res = first_modul_main(country_id,skp,products,duties,2035,0.05)
+    print(res)
