1a7f9f38f86c12b6d3865082dc147942f7bad03b
Jobir Umirov <jobirumirovmoliya@gmail.com>
2022-10-28 16:10:53 +0500

    debug false ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,13 +17,13 @@ sleep 10
 echo "Apply database migrations"
 python3 manage.py migrate
 
-echo "collectstatic"
-python3 manage.py collectstatic --no-input
+# echo "collectstatic"
+# python3 manage.py collectstatic
 
 echo "initadmin"
 python3 create_admin.py
 
 echo "Starting server"
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver --insecure 0.0.0.0:8000
 
 exec "$@"
